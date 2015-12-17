e6b64f287a097830ccc406b32dac4cd7e586d253
Yohei Onishi <vivre214@gmail.com>
2015-12-17 22:04:18 +0900

    add conv

--- app/models/Sample.scala
+++ app/models/Sample.scala
@@ -8,32 +8,32 @@ import slick.driver.JdbcProfile
 import scala.concurrent.ExecutionContext
 
 class Color extends Enumeration {
-  val Blue = Value
-  val Red = Value
-  val Green = Value
+  type Color = Value
+  val Blue = Value("Blue")
+  val Red = Value("Red")
+  val Green = Value("Green")
 }
 object Color extends Color
 
-case class Sample(name:String, v:Int, c:Color)
+case class Sample(name:String, id:Int, c:Color)
 
 // Schemas
 @Singleton
 class ColorDao @Inject()(dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
   val dbConfig = dbConfigProvider.get[JdbcProfile]
-
   import dbConfig._
   import driver.api._
 
-  class SampleTable(tag: Tag) extends Table[Sample](tag, "STATS") {
-    def name  = column[String]("name")
-    def v     = column[Int]("v")
+  class SampleTable(tag: Tag) extends Table[Sample](tag, "Sample") {
+    def name  = column[String]("NAME")
+    def id    = column[Int]("ID")
     def color = column[Color]("COLOR")
-    def * = (name, v, color) <> (Sample.tupled, Sample.unapply)
+    def * = (name, id, color) <> (Sample.tupled, Sample.unapply)
   }
 
-  def enumIdMapper(enum: Enumeration) = MappedColumnType.base[enum.Value, Int](
-    e => e.id,
-    i => enum.apply(i)
+  def enumStringMapper(enum: Enumeration) = MappedColumnType.base[enum.Value, String](
+    e => e.toString,
+    s => enum.withName(s)
   )
-  implicit val colorMapper = enumIdMapper(Color)
+  implicit val colorMapper = enumStringMapper(Color)
 }
